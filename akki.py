689dbf8da39313275fe6b6396eafc3967837f4b2
akkii14 <149265170+akkii14@users.noreply.github.com>
2023-11-03 00:16:24 +0530

    Add files via upload

---
+++ akki.py
@@ -0,0 +1,3 @@
+user = input('What is your weight: ')
+weight_kg = int(user) * 0.45
+print(weight_kg)
---
+++ database.py
@@ -0,0 +1,14 @@
+import mysql.connector
+try:
+	conn = mysql.connector.connect(
+		user='root',
+		password='geek',
+		host='localhost',
+		port=3306
+	)
+	if(conn.is_connected()):
+		print('Connected')
+except:
+	print('Unable to connect')
+
+conn.close()
